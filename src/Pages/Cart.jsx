e0806434e50ab5910f0a2fb9ca6e05a1b71e67fb
youal woldemariam <youalwol@yahoo.com>
2024-06-21 15:58:01 +0200

    Implemented a "Thank you for your order!" message after checkout in Cart.js, and Updated cart icon to reflect the current number of items in the cart.

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,44 +1,70 @@
-// Cart.js
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ShopContext } from "../Components/Context/ShopContext";  
 import { Products } from '../Products';
 import '../Pages/Css/Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTruck } from '@fortawesome/free-solid-svg-icons';
 import CartItem from '../Pages/CartItem';
-import { useNavigate } from "react-router-dom";
+
 
 const Cart = () => {
-  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount, clearCart } = useContext(ShopContext);
   
+  const [loading, setLoading] = useState(false); // Tillståndsvariabel för laddning
+  const [checkoutComplete, setCheckoutComplete] = useState(false); // Tillståndsvariabel för att visa "Tack"
   
+
   const totalAmount = getTotalCartAmount();
-  const navigate = useNavigate();
+
+  const handleCheckout = () => {
+    setLoading(true); // Sätt laddning till true när användaren klickar på Checkout
+
+    // Simulera en laddning med setTimeout
+    setTimeout(() => {
+      setLoading(false); // Sätt laddning till false efter simuleringen är klar
+      setCheckoutComplete(true); // Visa "Tack"-meddelande
+      clearCart(); // Anropa clearCart för att återställa varukorgen
+      // Simulera en navigering efter 2 sekunder
+      
+    }, 2000); // Simulerar en laddning i 2 sekunder (2000 millisekunder)
+  };
 
   return (
     <div className='cart'>
-      
-     
-      <div className="cartitems">
-        {Object.keys(cartItems).map(itemId => {
-          const quantity = cartItems[itemId];
-          if (quantity > 0) {
-            const product = Products.find(product => product.id === parseInt(itemId));
-            return <CartItem key={product.id} data={product} quantity={quantity} />;
-          }
-          return null;
-        })}
-      </div>
-
-      {totalAmount > 0 ? (
-        <div className='checkout'>
-          <p>Total: ${totalAmount}</p>
-          <button onClick={() => navigate("/Home")}>Continue Shopping</button>
-          <button className='checkout-button'>Checkout</button>
+      {!checkoutComplete ? (
+        <div className="cartitems">
+          {Object.keys(cartItems).map(itemId => {
+            const quantity = cartItems[itemId];
+            if (quantity > 0) {
+              const product = Products.find(product => product.id === parseInt(itemId));
+              return <CartItem key={product.id} data={product} quantity={quantity} />;
+            }
+            return null;
+          })}
         </div>
       ) : (
-        <h1>the cart is Empty</h1>
+        <div className="thank-you-message">
+          <h1>Tack för din beställning!</h1>
+          <FontAwesomeIcon icon={faTruck} className="truck-icon" />
+        </div>
       )}
-      
 
+      {!checkoutComplete && totalAmount > 0 ? (
+        <div className='checkout'>
+          <p>Total: ${totalAmount}</p>
+         
+          {loading ? (
+            <div className="loading-spinner">
+              <p>Loading...</p>
+             
+            </div>
+          ) : (
+            <button className='checkout-button' onClick={handleCheckout}>Checkout</button>
+          )}
+        </div>
+      ) : (
+        <h1>Varukorgen är tom</h1>
+      )}
     </div>
   );
 };
